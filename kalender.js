8b57a4f0bcb8a290e9a9077fcaee74ab7e4850eb
Egor Koschel <egor_koschel@teilnehmer.cdemy.de>
2023-08-08 08:48:25 +0200

    add commentare

--- kalender.js
+++ kalender.js
@@ -1,39 +1,46 @@
+//set global date for all functions
 let globalDate = new Date();
+//for religious holidays we get the date from funktion "getFeiertag()"
 let easterDate;
 let karfreitag;
 let christiHimmelfahrt;
 let pfingstMontag;
 let fronleichnam;
+//holidays with fixed date
 let neuesYahr = new Date (globalDate.getFullYear(), 0, 1);
 let tagDerArbeit = new Date (globalDate.getFullYear(), 4, 1)
 let tagDerEinheit = new Date (globalDate.getFullYear(), 9, 3);
 let weihnachtstag1 = new Date (globalDate.getFullYear(), 11, 25);
 let weihnachtstag2 = new Date (globalDate.getFullYear(), 11, 26);
-
+//run all functions on load
 window.onload = getFeiertag(), kopfJS(), infotextJS(), weekInMonthJS(), feiertagYesNoJS(), kalendarblattJS();
 console.log("globalDate:" + globalDate);
 
+// show today when you click on header
 let headButton = document.getElementById("headButton");
     headButton.addEventListener("click", function () {
         globalDate = new Date();
         kopfJS(), infotextJS(), weekInMonthJS(), feiertagYesNoJS(), kalendarblattJS();
     });
 
+// function draws header
 function kopfJS()
 {
     let today = globalDate;
     let dateD = today.getDate();
     let monthD = today.getMonth();
     let year = today.getFullYear();
+    //create array with names of month
     const monthNames = ["Januar","Februar","März","April","Mai","Juni","Juli","August","September","Oktober","November","Dezember"];
     let monthText = (monthNames[monthD]);
+    // adding text with date ant month in HTML
     document.getElementById('dateD').textContent = dateD;
     document.getElementById('monthD').textContent = monthText;
     document.getElementById('year').textContent = year;
     console.log ("kopfDate " + globalDate);
 }
 
-
+ // calendar function
 function kalendarblattJS() {
     let today = globalDate;
     // get ID
@@ -42,6 +49,7 @@ function kalendarblattJS() {
     let prevBtn = document.getElementById("prev-btn");
     let nextBtn = document.getElementById("next-btn");
 
+    //function draws calender
     function renderCalendar() {
 
         let firstDayNextMonth = 0;
@@ -80,6 +88,7 @@ function kalendarblattJS() {
         // get the day of the week of the last day of the current month
         let lastDayOfWeek = lastDayOfMonth.getDay();
 
+        //change sunday number from 0 to 7
         if (lastDayOfWeek == 0) {
             lastDayOfWeek = 7;
         }
@@ -121,7 +130,7 @@ function kalendarblattJS() {
         
     }
 
-    // create a day cell with a number
+    // function for create a day cell with a number
     function createDayCell(day) {
         // create a day cell ID
         const dayCell = document.createElement("div");
@@ -139,14 +148,14 @@ function kalendarblattJS() {
             dayCell.classList.add("day");
             }
 
-        // adding the class "today" in style.css and show today
+        // show today with style "today"
         if (day==globalDate.getDate() && currentMonth==globalDate.getMonth() && currentYear==globalDate.getFullYear() ){
             dayCell.classList.add("today");
         }
-        // set cell content text
+        // set text in cell
         dayCell.textContent = day;
 
-        // display a message when you click on the current day
+        // display a message with info when you click on the current day
         dayCell.addEventListener("click", function () {
             globalDate = new Date(today.getFullYear(), today.getMonth(), day);
             console.log ("globalDate: " + globalDate);
@@ -164,9 +173,9 @@ function kalendarblattJS() {
     function createGrayCell(day) {
             // create a day cell ID
             const dayCell = document.createElement("div");
-            // adding the class "grayday" in style.css
+            // adding style
             dayCell.classList.add("grayday");
-            // set cell content text
+            // text in cell
             dayCell.textContent = day;
             // returning the created cell element
             return dayCell;
@@ -175,9 +184,9 @@ function kalendarblattJS() {
     function createFeiertagCell(day) {
         // create a day cell ID
         const dayCell = document.createElement("div");
-        // adding the class "grayday" in style.css
+        // adding style
         dayCell.classList.add("feiertag");
-        // set cell content text
+        // set text in cell
         dayCell.textContent = day;
         // returning the created cell element
         return dayCell;
@@ -188,20 +197,21 @@ function kalendarblattJS() {
 
     // buttons
     prevBtn.addEventListener("click", function () {
-        // when you click the "Zurück" button, we decrease the month of the current date by 1 and redraw the calendar
+        // when you click "Zurück" button, we decrease the month of the current date by 1 and redraw the calendar
         today.setMonth(today.getMonth() - 1);
         kopfJS(), infotextJS(), weekInMonthJS(), feiertagYesNoJS(), getFeiertag(), renderCalendar();
         console.log ("globalDate fater click on previous or next month ", globalDate);
     });
 
     nextBtn.addEventListener("click", function () {
-        // when you click the "Weiter" button, increase the month of the current date by 1 and redraw the calendar
+        // when you click "Weiter" button, increase the month of the current date by 1 and redraw the calendar
         today.setMonth(today.getMonth() + 1);
         kopfJS(), infotextJS(), weekInMonthJS(), feiertagYesNoJS(), getFeiertag(), renderCalendar();
         console.log ("globalDate fater click on previous or next month ", globalDate);
     });
 }
 
+//get correct text to describe the day
 function infotextJS()
 
 {
@@ -210,10 +220,13 @@ function infotextJS()
     let weekdayD = today.getDay();
     let monthD = today.getMonth();
     let year = today.getFullYear();
+    //set array with names of month
     const monthNames = ["Januar","Februar","März","April","Mai","Juni","Juli","August","September","Oktober","November","Dezember"];
-    let monthText = (monthNames[monthD])
+    let monthText = (monthNames[monthD]);
+    //set array with names days of week
     const weekNames = [ "Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag","Samstag"];
     let weekText = (weekNames[weekdayD]);
+    //adding text in HTML
     document.getElementById('textDate').textContent = dateD;
     document.getElementById('textMonth').textContent = monthText;
     document.getElementById('textMonth2').textContent = monthText;
@@ -221,8 +234,11 @@ function infotextJS()
     document.getElementById('textWeekday2').textContent = weekText;
     document.getElementById('textMonth').textContent = monthText;
     document.getElementById('textYear').textContent = year;
+    document.getElementById('historieDate').textContent = dateD;
+    document.getElementById('historieMonth').textContent = monthText;
 }
 
+//function for set number of day of week in current month
 function weekInMonthJS()
 
 {
@@ -231,10 +247,11 @@ function weekInMonthJS()
     const currentDay = today.getDate();
     const firstWeekdayOfMonth = new Date(today);
     firstWeekdayOfMonth.setDate(1);
-
+    //search dates from beginning of month to determine first cuttent day of week in the month
     while (firstWeekdayOfMonth.getDay() !== weekdayD) {
         firstWeekdayOfMonth.setDate(firstWeekdayOfMonth.getDate()+1);
         }
+        //count number of the current day of week in the month, if current day is Sunday
         if (weekdayD == 0){
             const numberWeekday = Math.ceil((currentDay - firstWeekdayOfMonth.getDate() + 7)/7);
             const numberWeekdayName = [ "erste", "zweite", "dritte", "vierte", "fünfte" ];
